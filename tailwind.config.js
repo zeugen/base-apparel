73d21c44e41966070632ffc34de712703cfad1cf
Zeugen <zeugen@users.noreply.github.com>
2021-04-17 00:58:54 +0300

    welcome page fully styled

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        csFont:['Josefin Sans']
+      },
+      colors:{
+        desaturatedRed:" hsl(0, 36%, 70%)",
+        softRed:"hsl(0, 93%, 68%)",
+        grayishRed:" hsl(0, 6%, 24%)",
+        gradOneFrom:"hsl(0, 0%, 100%)",
+        gradOneTo:"hsl(0, 100%, 98%)",
+        gradTwoFrom:"hsl(0, 80%, 86%)",
+        gradTwoTo:"hsl(0, 74%, 74%)"
+      }
+    },
   },
   variants: {
     extend: {},
